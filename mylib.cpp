04f91317addb08f2b9ca75e0834dd813b51c59a3
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-12-05 11:21:22 +0100

    TEST: Force-link g_forceLink global.

--- mylib.cpp
+++ mylib.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+
+void InitializationTask() {
+    printf("Library loading.\n");
+    return 42;
+}
+
+__attribute__((used))
+int g_forceLink = InitializationTask();
+
+
 int mylibFunction() {
   printf("Hello world\n");
   return 42;
