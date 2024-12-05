187ab1ad46c014b2fa52f2e336456535f504a2a5
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-12-05 11:16:01 +0100

    Add cleanup and comment-out dependency listing.

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
+echo Cleaning up...
+rm *.o
+rm *.so
+rm mainApp
+
 echo Building libmylib.so...
 g++ -fPIC -c mylib.cpp -o mylib.o
 g++ -shared -o libmylib.so mylib.o
 
-echo libmylib.so dependencies:
-ldd libmylib.so
-
+#echo libmylib.so dependencies:
+#ldd libmylib.so
 
 echo Building mainApp...
 g++ main.cpp -L. -lmylib -o mainApp
 
-echo mainApp dependencies:
-ldd mainApp
+#echo mainApp dependencies:
+#ldd mainApp
 
 echo Running mainApp:
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
