15b2e3144aca65526425c4654707d501b1ec0100
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-15 16:50:48 +0100

    .

--- glibc/mylib.c
+++ glibc/mylib.c
@@ -7,8 +7,9 @@
 int compute_sum (int a, int b) {
     printf("mylib built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
 
-    printf("Performing dynamic memory allocation...\n");
+    printf("Testing dynamic memory allocation...\n");
     std::vector<uint8_t> buffer(1024*1014, (uint8_t)0);
+    printf("[success]\n\n");
 
     printf("Computing the sum of %i and %i ...\n", a, b);
     int sum = a + b;
