04ca0423640eb0f302bc61bd06ef6af3700c18f0
Fredrik Orderud <forderud@gmail.com>
2025-03-15 11:13:26 +0100

    .

--- dlopen/mylib.c
+++ dlopen/mylib.c
@@ -5,7 +5,7 @@
 int compute_sum (int a, int b) {
     printf("mylib built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
     
-    printf("Computing the sum of %i and %i ...\n");
+    printf("Computing the sum of %i and %i ...\n", a, b);
     int sum = a + b;
     printf("The sum is %i\n", sum);
     return sum;
