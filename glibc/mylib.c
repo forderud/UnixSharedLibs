4accdf6fbf6ec3ec1d7d6f897986003ab98bd46c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-15 16:54:02 +0100

    Add file I/O test.

--- glibc/mylib.c
+++ glibc/mylib.c
@@ -1,5 +1,7 @@
 #include <cstdint>
+#include <fstream>
 #include <stdio.h>
+#include <string>
 #include <vector>
 #include "mylib.h"
 
@@ -7,9 +9,19 @@
 int compute_sum (int a, int b) {
     printf("mylib built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
 
-    printf("Testing dynamic memory allocation...\n");
-    std::vector<uint8_t> buffer(1024*1014, (uint8_t)0);
-    printf("[success]\n\n");
+    {
+        printf("Testing dynamic memory allocation...\n");
+        std::vector<uint8_t> buffer(1024*1014, (uint8_t)0);
+        printf("[success]\n\n");
+    }
+    {
+        printf("Testing file I/O...\n");
+        std::ifstream file("document.txt");
+        std::string line;
+        while (std::getline(file, line))
+            printf("file content: %s\n", line.c_str());
+        printf("[success]\n\n");
+    }
 
     printf("Computing the sum of %i and %i ...\n", a, b);
     int sum = a + b;
