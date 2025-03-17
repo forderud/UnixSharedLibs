1aae6368076e2c9329e06633b15c1e21a0aac436
Fredrik Orderud <forderud@gmail.com>
2025-03-17 07:59:23 -0400

    .

--- glibc/build.sh
+++ glibc/build.sh
@@ -7,7 +7,7 @@ rm mainApp
 # explicity set C++11 for compatibility with really old compilers that default to C++98
 
 echo ""
-echo Building mystaticlib.a...
+echo Building static lib...
 g++ -std=c++11 -fPIC -c mystaticlib.cpp -o mystaticlib.o
 ar rcs libmystaticlib.a mystaticlib.o
 
@@ -15,20 +15,20 @@ ar rcs libmystaticlib.a mystaticlib.o
 #nm libmystaticlib.a
 
 echo ""
-echo Building libmysharedlib.so...
+echo Building shared lib...
 g++ -std=c++11 -fPIC -c mysharedlib.cpp -o mysharedlib.o
 g++ -shared -static-libgcc -static-libstdc++ mysharedlib.o libmystaticlib.a -o libmysharedlib.so
 
 echo libmysharedlib.so dependencies:
 ldd libmysharedlib.so
 
-echo Building mainApp...
+echo Building application...
 g++ main.cpp -DUSE_DLOPEN -L. -ldl -lmysharedlib -o mainApp
 
 #echo mainApp dependencies:
 #ldd mainApp
 
 echo ""
-echo Running mainApp:
+echo Running application...
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 ./mainApp
