16394d9f20347bc90249c5568495f2f0447eeb17
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-17 10:46:08 +0100

    Start adding a static library to the mix.

---
+++ glibc/mystaticlib.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include "mystaticlib.hpp"
+
+
+int compute_sum_static (int a, int b) {
+    printf("mystaticlib built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
+
+    {
+        printf("Testing dynamic memory allocation...\n");
+        std::vector<uint8_t> buffer(1024*1014, (uint8_t)0);
+        assert(buffer[1024] == 0);
+        printf("[success]\n\n");
+    }
+    {
+        printf("Testing file I/O...\n");
+        std::ifstream file("document.txt");
+        std::string line;
+        while (std::getline(file, line))
+            printf("file content: %s\n", line.c_str());
+        printf("[success]\n\n");
+    }
+
+    printf("Computing the sum of %i and %i ...\n", a, b);
+    int sum = a + b;
+    printf("The sum is %i\n", sum);
+    return sum;
+}
