7e8b8ca1d01ca7b3921f43d9a5b76d8df8471578
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 15:38:58 +0200

    .

--- mac-embed-file/build.sh
+++ mac-embed-file/build.sh
@@ -4,7 +4,7 @@ rm -f *.o stub.c mainApp
 # convert embed_example.txt into an object file to binary embedding
 touch stub.c
 g++ -c stub.c -o stub.o
-ld -r -sectcreate __TEXT embed_example embed_example.txt -o embed_example.o stub.o
+ld  stub.o -r -sectcreate __TEXT embed_example embed_example.txt -o embed_example.o
 
 echo Building mainApp...
 g++ main.cpp embed_example.o -o mainApp
