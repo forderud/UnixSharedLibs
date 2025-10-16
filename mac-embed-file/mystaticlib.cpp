145d4a0544fd16118e49a4dfba1ab97ae2a7050c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 20:53:17 +0200

    fixes

--- mac-embed-file/mystaticlib.cpp
+++ mac-embed-file/mystaticlib.cpp
@@ -7,6 +7,6 @@ void print_embedded_file (const char* segment_name) {
     unsigned long embed_example_size = 0;
     const uint8_t* embed_example_start = getsectiondata(&_mh_execute_header, "__TEXT", segment_name, &embed_example_size);
 
-    printf("Content of %s (%u bytes):\n", segment_name, embed_example_size);
+    printf("Content of %s (%u bytes):\n", segment_name, (unsigned int)embed_example_size);
     printf("%.*s\n", (int)embed_example_size, embed_example_start); // specify size since file content is not null-terminated
 }
