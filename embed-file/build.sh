21a65a4b74e289860858d97d9636ed4782675497
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 14:20:00 +0200

    Add file embedding example

---
+++ embed-file/build.sh
@@ -0,0 +1,14 @@
+echo Cleaning up...
+rm -f *.o mainApp
+
+# convert example.txt into an object file to binary embedding
+ld -r -b binary -o example.o example.txt
+
+echo Building mainApp...
+g++ main.cpp -L. example.o -o mainApp
+
+echo ""
+echo Running mainApp:
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+./mainApp
+
