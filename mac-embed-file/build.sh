5556cb1f7de6c728912df6aa2d4b7463363f773e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 21:19:27 +0200

    .

--- mac-embed-file/build.sh
+++ mac-embed-file/build.sh
@@ -4,12 +4,12 @@ rm -f *.o *.a mainApp
 echo Building static library that embeds a binary file...
 g++ -c mystaticlib.cpp -o lib_sources.o
 ld  lib_sources.o -r -sectcreate __TEXT embed_example embed_example.txt -o mystaticlib.o
-libtool -static mystaticlib.o -o mystaticlib.a
+libtool -static mystaticlib.o -o libmystatic.a
 
 
 echo ""
 echo Building application that links in the static library and accesses the embedded file...
-g++ main.cpp mystaticlib.a -o mainApp
+g++ main.cpp -L. -lmystatic -o mainApp
 
 echo ""
 echo Running application:
