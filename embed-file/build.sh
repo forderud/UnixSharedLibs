f2431142247ad6970424185aaa0d2755a6c76c48
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 14:29:31 +0200

    .

--- embed-file/build.sh
+++ embed-file/build.sh
@@ -2,7 +2,7 @@ echo Cleaning up...
 rm -f *.o mainApp
 
 # convert embed_example.txt into an object file to binary embedding
-ld -r -b binary -o embed_example.o embed_example.txt
+ld -r -b binary embed_example.txt -o embed_example.o 
 
 echo Building mainApp...
 g++ main.cpp embed_example.o -o mainApp
