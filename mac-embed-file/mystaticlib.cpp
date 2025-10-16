8bf529887ba320fb12b252d6e819685b4ae327f8
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 21:13:49 +0200

    .

--- mac-embed-file/mystaticlib.cpp
+++ mac-embed-file/mystaticlib.cpp
@@ -5,8 +5,9 @@
 
 void print_embedded_file (const char* section_name) {
     // access embedded binary file
+    const char segment_name[] = "__TEXT";
     unsigned long embed_example_size = 0;
-    const uint8_t* embed_example_start = getsectiondata(&_mh_execute_header, /*segment name*/"__TEXT", section_name, &embed_example_size);
+    const uint8_t* embed_example_start = getsectiondata(&_mh_execute_header, segment_name, section_name, &embed_example_size);
 
     printf("Content of %s (%u bytes):\n", section_name, (unsigned int)embed_example_size);
     printf("%.*s\n", (int)embed_example_size, embed_example_start); // specify size since file content is not null-terminated
