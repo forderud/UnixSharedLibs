3cb10b2e8119fbaef12fa615e5cc9abbcd0384f5
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 14:28:08 +0200

    rename embedded file

--- embed-file/build.sh
+++ embed-file/build.sh
@@ -1,14 +1,13 @@
 echo Cleaning up...
 rm -f *.o mainApp
 
-# convert example.txt into an object file to binary embedding
-ld -r -b binary -o example.o example.txt
+# convert embed_example.txt into an object file to binary embedding
+ld -r -b binary -o embed_example.o embed_example.txt
 
 echo Building mainApp...
-g++ main.cpp example.o -o mainApp
+g++ main.cpp embed_example.o -o mainApp
 
 echo ""
 echo Running mainApp:
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 ./mainApp
-
