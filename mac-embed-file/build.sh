e3fc39ad1023315d6267fb904b533ff45627672c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-16 21:01:21 +0200

    .

--- mac-embed-file/build.sh
+++ mac-embed-file/build.sh
@@ -2,12 +2,11 @@ echo Cleaning up...
 rm -f *.o stub.c mainApp
 
 # convert embed_example.txt into an object file to binary embedding
-touch stub.cpp
-g++ -c stub.cpp -o stub.o
+g++ -c mystaticlib.cpp -o stub.o
 ld  stub.o -r -sectcreate __TEXT embed_example embed_example.txt -o embed_example.o
 
 echo Building mainApp...
-g++ main.cpp mystaticlib.cpp embed_example.o -o mainApp
+g++ main.cpp embed_example.o -o mainApp
 
 echo ""
 echo Running mainApp:
