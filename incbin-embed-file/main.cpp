39e4f2d6a3016d6b764342494894562932225cd8
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-22 12:22:01 +0200

    .

--- incbin-embed-file/main.cpp
+++ incbin-embed-file/main.cpp
@@ -6,6 +6,6 @@ INCBIN(EmbedExample, "embed_example.txt");
 
 int main() {
     size_t fileSize = gEmbedExampleSize;
-    printf("%.*s\n", fileSize, gEmbedExampleData); // specify size since file content is not null-terminated
+    printf("%.*s\n", (int)fileSize, gEmbedExampleData); // specify size since file content is not null-terminated
     return 0;
 }
