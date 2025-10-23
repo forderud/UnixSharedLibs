180f0f5c8602a7b2eeb0e23c696df5ec0eb8dd28
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-23 11:24:50 +0200

    Document mach-o folder path.

--- mac-embed-file/mystaticlib.cpp
+++ mac-embed-file/mystaticlib.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// "mach-o" header folder path: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/mach-o/
 #include <mach-o/getsect.h> // for getsectiondata
 #include <mach-o/ldsyms.h>  // for _mh_execute_header
 
