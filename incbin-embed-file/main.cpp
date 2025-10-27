884cc4af9a49a6073983c6dae2f5f3985a59381f
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-27 13:39:50 +0100

    .

--- incbin-embed-file/main.cpp
+++ incbin-embed-file/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <string_view>
 #include "incbin.h"
+
+#define GetEmbeddedFile(NAME) \
+    (std::string_view((const char*)g##NAME##Data, g##NAME##Size))
 
 INCBIN(EmbedExample, "embed_example.txt");
 
-
 int main() {
-    size_t fileSize = gEmbedExampleSize;
-    printf("%.*s\n", (int)fileSize, gEmbedExampleData); // specify size since file content is not null-terminated
+    std::string_view file = GetEmbeddedFile(EmbedExample);
+    printf("Size   : %u\n", file.size());
+    printf("Content: %.*s\n", (int)file.size(), file.data()); // specify size since file content is not null-terminated
     return 0;
 }
