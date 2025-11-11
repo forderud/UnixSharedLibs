cef2098dceda8bd02555082c0a8994c7f36c0b15
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 21:03:28 +0100

    Convert minimal sample from C to C++

--- static-glibc/minimal/build.sh
+++ static-glibc/minimal/build.sh
@@ -3,14 +3,14 @@ cd "$(dirname "$0")"
 set -e # stop on first failure
 
 echo Cleaning up...
-rm -f mainApp
+rm -f *.o *.a *.so mainApp
 
 # Use non-default glibc
 link_flags=""
 link_flags="-L/usr/local/glibc-2.26/lib -I/usr/local/glibc-2.26/include -Wl,-rpath=/usr/local/glibc-2.26/lib -Wl,-dynamic-linker=/usr/local/glibc-2.26/lib/ld-linux-x86-64.so.2"
 
 echo Building shared C library..
-gcc -shared $link_flags sharedlib.c -L. -o libsharedLib.so
+g++ -shared $link_flags sharedlib.c -L. -o libsharedLib.so
 
 echo Transitive shared lib. dependencies:
 ldd libsharedlib.so
@@ -18,7 +18,7 @@ echo Direct shared lib. dependencies:
 readelf -d libsharedlib.so
 
 echo Building C application...
-gcc $link_flags main.c -L. -pthread -lsharedlib -o mainApp
+g++ $link_flags main.c -L. -pthread -lsharedlib -o mainApp
 
 echo mainApp dependencies:
 ldd mainApp
--- static-glibc/minimal/sharedlib.h
+++ static-glibc/minimal/sharedlib.h
@@ -1,3 +1,4 @@
 #pragma once
 
+extern "C" // to avoid name mangling
 int compute_sum (int a, int b);
