dc4964580ada40b489cdf453ef629b2bac684c2d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 21:00:19 +0100

    complete shared lib creation & testing

--- static-glibc/minimal/build.sh
+++ static-glibc/minimal/build.sh
@@ -9,12 +9,21 @@ rm -f mainApp
 link_flags=""
 link_flags="-L/usr/local/glibc-2.26/lib -I/usr/local/glibc-2.26/include -Wl,-rpath=/usr/local/glibc-2.26/lib -Wl,-dynamic-linker=/usr/local/glibc-2.26/lib/ld-linux-x86-64.so.2"
 
+echo Building shared C library..
+gcc -shared $link_flags sharedlib.c -L. -o libsharedLib.so
+
+echo Transitive shared lib. dependencies:
+ldd libsharedlib.so
+echo Direct shared lib. dependencies:
+readelf -d libsharedlib.so
+
 echo Building C application...
-gcc $link_flags main.c sharedlib.c -L. -pthread -o mainApp
+gcc $link_flags main.c -L. -pthread -lsharedlib -o mainApp
 
 echo mainApp dependencies:
 ldd mainApp
 
 echo ""
 echo Running C application...
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 ./mainApp
