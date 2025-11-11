5ad2776b7fe7ce168e44af20fd3bbb638027ee3b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 13:44:28 +0100

    stop re-building docker images every time.

--- static-glibc/Ubuntu-24.04.bat
+++ static-glibc/Ubuntu-24.04.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Building Ubuntu 24.04 image...
-docker build -f Ubuntu-24.04.Dockerfile --tag=forderud/unixsharedlibs:24.04 .
+::docker build -f Ubuntu-24.04.Dockerfile --tag=forderud/unixsharedlibs:24.04 .
 
 echo Building sample project...
 docker run --rm -v %cd%:/host forderud/unixsharedlibs:24.04 /host/build.sh || exit /b 1
