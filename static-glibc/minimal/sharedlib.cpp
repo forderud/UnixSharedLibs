7cb896af4a6fd39b4756199a5cbee37bcc067cc4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 21:08:49 +0100

    .

--- static-glibc/minimal/sharedlib.cpp
+++ static-glibc/minimal/sharedlib.cpp
@@ -1,9 +1,11 @@
+#include <cstdint>
 #include <stdio.h>
 #include "sharedlib.hpp"
 
 
 int compute_sum (int a, int b) {
     printf("sharedlib built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
+    printf("sharedlib built against libstdc++ %d\n", __GLIBCXX__); // was __GLIBCPP__ before 3.4.0
 
     int sum = a + b;
     printf("sharedlib returned %i\n", sum);
