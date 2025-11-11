b09181de663b928769392e42273471f03c88a30b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 13:38:49 +0100

    Extend script to also build the sample project in the container.
    The sample segfaults in main.cpp:20 when being run in the container.

--- static-glibc/Ubuntu-18.04.bat
+++ static-glibc/Ubuntu-18.04.bat
@@ -1,4 +1,7 @@
 @echo off
 
 echo Building Ubuntu 18.04 image...
-docker build -f Ubuntu-18.04.Dockerfile .
+docker build -f Ubuntu-18.04.Dockerfile --tag=forderud/unixsharedlibs:18.04 .
+
+echo Building sample project...
+docker run --rm -v %cd%:/host forderud/unixsharedlibs:18.04 /host/build.sh || exit /b 1
