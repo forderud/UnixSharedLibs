cc0ca5656fa71089ce1fdfd25b525f7553db8c9e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-11 20:25:56 +0100

    Extend sample with threading.

--- static-glibc/minimal/build.sh
+++ static-glibc/minimal/build.sh
@@ -9,7 +9,7 @@ link_flags=""
 link_flags="-L/usr/local/glibc-2.26/lib -I/usr/local/glibc-2.26/include -Wl,-rpath=/usr/local/glibc-2.26/lib -Wl,-dynamic-linker=/usr/local/glibc-2.26/lib/ld-linux-x86-64.so.2"
 
 echo Building C application...
-gcc $link_flags main.c -L. -o mainApp
+gcc $link_flags main.c -L. -pthread -o mainApp
 
 echo mainApp dependencies:
 ldd mainApp
--- static-glibc/minimal/main.c
+++ static-glibc/minimal/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <assert.h>
+#include <pthread.h>
 
+
+static void* thread_function(void *arg) {
+    printf("Hello from thread!\n");
+    return NULL;
+}
 
 int main () {
     printf("executable built against glibc %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
+
+    size_t job = 0;
+    pthread_t thread = {};
+    pthread_create(&thread, NULL, thread_function, &job);
+    pthread_join(thread, NULL);
+
     return 0;
 }
