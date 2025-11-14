ecff4e0e823be7521d7c61212008c127bacac812
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-14 12:22:08 +0100

    .

--- static-libstdc++/build.sh
+++ static-libstdc++/build.sh
@@ -5,14 +5,15 @@ set -e # stop on first failure
 echo Cleaning up...
 rm -f *.so a.out
 
-# build shared lib.
-# links to static libstdc++ to avoid run-time dependency
+echo "Building shared lib..."
+# Link to static libstdc++ to avoid run-time dependency.
+# Use --exclude-libs,ALL to avoid exporting symbols from external libraries, like libstdc++
 g++ -fPIC -static-libstdc++ -shared libfoo.cpp -o libfoo.so -Wl,--exclude-libs,ALL -pthread
 
-# build app that links to shared lib
+echo "Building app that links to shared lib..."
 g++ main.cpp -L. -lfoo -Wl,-rpath=. -pthread -O2
 
 
 echo ""
-echo "Running application that will segfault on startup..."
+echo "Running application (will segfault)..."
 ./a.out
