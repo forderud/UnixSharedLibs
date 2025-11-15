c66a78475c8c45343d7e78546ac9ed154c3505c7
Fredrik Orderud <forderud@gmail.com>
2025-11-15 15:31:55 +0100

    .

--- Ubuntu-18.04.bat
+++ Ubuntu-18.04.bat
@@ -4,7 +4,7 @@ echo Building Ubuntu 18.04 image...
 ::docker build -f Ubuntu-18.04.Dockerfile --tag=forderud/unixsharedlibs:18.04 .
 
 echo Building sample project...
-docker run --rm -v %cd%:/host forderud/unixsharedlibs:18.04 /host/build.sh || exit /b 1
+docker run --rm -v %cd%:/host forderud/unixsharedlibs:18.04 /host/libc-deps/static-glibc/build.sh || exit /b 1
 
 :: Open an interactive bash shell inside the container
 ::docker run --rm -it forderud/unixsharedlibs:18.04 sh
