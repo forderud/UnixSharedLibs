34de28315a2514c10ed84c5a7646a00cc8cef5c4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-16 15:42:50 +0100

    .

--- libc-deps/bundle-deps/build.sh
+++ libc-deps/bundle-deps/build.sh
@@ -10,17 +10,17 @@ echo Building shared C++ lib...
 g++ -fPIC -c mysharedlib.cpp -o mysharedlib.o
 g++ -shared -pthread mysharedlib.o -o libmysharedlib.so
 
-echo Transitive shared lib. dependencies:
-ldd libmysharedlib.so
-echo Direct shared lib. dependencies:
-readelf -d libmysharedlib.so
-
 # copy libstdc++ library to current folder
 cp /lib/x86_64-linux-gnu/libstdc++.so.6 .
 cp /lib/x86_64-linux-gnu/libc.so.6 .
 
 # Add current dir to library search path
 export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
+
+echo Transitive shared lib. dependencies:
+ldd libmysharedlib.so
+echo Direct shared lib. dependencies:
+readelf -d libmysharedlib.so
 
 echo libstdc++.so.6 dependencies:
 ldd libstdc++.so.6
