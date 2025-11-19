1bb38de9c3599dceeb9672bac202256a1da3be94
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-19 19:01:29 +0100

    copy first

--- bundle-deps/build.sh
+++ bundle-deps/build.sh
@@ -5,13 +5,13 @@ set -e # stop on first failure
 echo Cleaning up...
 rm -f *.o *.so a.out
 
+echo Copying C/C++ library dependencies to current folder...
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 .
+
 echo ""
 echo Building shared C++ lib...
 g++ -fPIC -c mysharedlib.cpp -o mysharedlib.o
 g++ -shared -pthread mysharedlib.o -Wl,-rpath=. -o libmysharedlib.so
-
-echo Copying C/C++ library dependencies to current folder...
-cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 .
 
 # Add current dir to library search path
 export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
