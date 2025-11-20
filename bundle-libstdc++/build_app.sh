6c90d3260bece446961f5bcef7d912e71fe658c0
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-20 14:50:30 +0100

    split script in two parts

---
+++ bundle-libstdc++/build_app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+set -e # stop on first failure
+
+link_flags="-Wl,-rpath=\$ORIGIN"
+
+# Alternative mechanism for using bundled libstdc++ version
+#export LD_LIBRARY_PATH=`pwd`
+
+#echo Shared lib. dependencies:
+#ldd libModernStuff.so
+#readelf -d libModernStuff.so
+
+echo Building C++ application with old compiler with outdated libstdc++...
+# Passing -nodefaultlibs to avoid auto-linking to GCC-bundled libstdc++ (/usr/lib/gcc/x86_64-linux-gnu/7/libstdc++.so)
+# TODO: Switch to -nostdlib++ after upgrading to GCC 13
+# Add -Wl,--trace to list linked-to libraries
+g++-7 main.cpp -nodefaultlibs -L. -lc `pwd`/libstdc++.so.6 -lModernStuff $link_flags
+
+#echo executable dependencies:
+#ldd a.out
+
+echo ""
+echo Running application...
+./a.out
--- bundle-libstdc++/build_lib.sh
+++ bundle-libstdc++/build_lib.sh
@@ -15,22 +15,6 @@ echo Building shared C++ lib that uses a recent libstdc++ version...
 g++-13 -fPIC -c ModernStuff.cpp -o ModernStuff.o
 g++-13 -shared -pthread $link_flags -o libModernStuff.so ModernStuff.o
 
-# Alternative mechanism for using bundled libstdc++ version
-#export LD_LIBRARY_PATH=`pwd`
-
 #echo Shared lib. dependencies:
 #ldd libModernStuff.so
 #readelf -d libModernStuff.so
-
-echo Building C++ application with old compiler with outdated libstdc++...
-# Passing -nodefaultlibs to avoid auto-linking to GCC-bundled libstdc++ (/usr/lib/gcc/x86_64-linux-gnu/7/libstdc++.so)
-# TODO: Switch to -nostdlib++ after upgrading to GCC 13
-# Add -Wl,--trace to list linked-to libraries
-g++-7 main.cpp -nodefaultlibs -L. -lc `pwd`/libstdc++.so.6 -lModernStuff $link_flags
-
-#echo executable dependencies:
-#ldd a.out
-
-echo ""
-echo Running application...
-./a.out
